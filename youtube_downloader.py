1988762751655d74d098b7e249ec9045e4b2f907
Finn <32446445+Delafinn@users.noreply.github.com>
2022-12-24 12:02:44 -0700

    Update youtube_downloader.py
    Added audio_only function to youtube_downloader.py
    Added high_resolution function to youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,22 +3,38 @@ import time
 import sys
 from pytube import YouTube
 
-
 while True:
 
     URL = input("""Enter the YouTube video URL\n
     Example: https://www.youtube.com/watch?v=dQw4w9WgXcQ \n:""")
     if URL in ("","exit"):
         sys.exit()
-    yt = YouTube(URL)
-    stream = yt.streams.get_highest_resolution()
 
-    print(f"the video title is {yt.title.upper()}")
-    download_q = input("Do you wish to download the video?").lower()
-    if download_q in ("no","n"):
-        sys.exit()
-    if download_q in ("yes","y"):
+    yt = YouTube(URL)
+
+    def high_res():
+        """downloads highest resolution video"""
+        stream = yt.streams.get_highest_resolution()
         stream.download()
         print(f"Video {yt.title.upper()} has been downloaded!")
         time.sleep(1.1)
         input("press any key to continue.")
+
+    def audio_only():
+        """downloads audio version"""
+        stream = yt.streams.get_audio_only()
+        stream.download()
+        print(f"Video {yt.title.upper()} has been downloaded!")
+        time.sleep(1.1)
+        input("press any key to continue.")
+
+    print(f"the video title is {yt.title.upper()}")
+    download_q = input("Do you wish to download the video?").lower()
+    if download_q in ("no","n","","exit"):
+        sys.exit()
+    if download_q in ("yes","y"):
+        audio_q = input("Do you wish to download the audio version only?").lower()
+        if audio_q in ("yes","y"):
+            audio_only()
+        else:
+            high_res()
