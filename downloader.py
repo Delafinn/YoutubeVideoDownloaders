988e8e84e9ba5994273fec4c6557414e708e960d
Jirayu Kaewsing <strixz.self@gmail.com>
2022-12-28 13:19:20 +0700

    Move playlist dowload function into downloader.py

--- downloader.py
+++ downloader.py
@@ -1,4 +1,3 @@
-import time
 import pytube
 
 def download_high_res(video: pytube.YouTube, path: str) -> None:
@@ -10,7 +9,7 @@ def download_high_res(video: pytube.YouTube, path: str) -> None:
     stream = video.streams.get_highest_resolution()
     stream.download(path)
 
-    print(f'Video {video.title.upper()} has been downloaded!')
+    print(f'Video: {video.title} has been downloaded!')
 
 
 def download_audio(video: pytube.YouTube, path: str) -> None:
@@ -22,4 +21,30 @@ def download_audio(video: pytube.YouTube, path: str) -> None:
     stream = video.streams.get_audio_only()
     stream.download(path)
 
-    print(f'Video {video.title.upper()} has been downloaded!')
+    print(f'Video: {video.title} has been downloaded!')
+
+
+def download_playlst_high_res(playlist: pytube.Playlist, path: str) -> None:
+
+    '''
+        Download all video from the given playlist with the highest resolution  
+    '''
+    
+    for vid in playlist.videos:
+        vid.streams.get_highest_resolution().download(path)
+        print(f"Video: {vid.title} has been downloaded!")
+
+    print('Download complete!')
+
+
+def download_playlst_audio(playlist: pytube.Playlist, path: str) -> None:
+    
+    '''
+        Download all video from the given playlist as audio files.
+    '''
+
+    for vid in playlist.videos:
+        vid.streams.get_audio_only().download(path)
+        print(f"Video: {vid.title} has been downloaded!")
+
+    print('Download complete!')
