de61f26574c7b0fb19470a3a16eb8790bb8be372
Austin <105258719+Austin-540@users.noreply.github.com>
2022-12-28 17:20:22 +1300

    added ability to specify directory

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -15,7 +15,7 @@ while True:
     def high_res():
         """downloads highest resolution video"""
         stream = yt.streams.get_highest_resolution()
-        stream.download()
+        stream.download(specify_directory)
         print(f"Video {yt.title.upper()} has been downloaded!")
         time.sleep(1.1)
         input("press any key to continue.")
@@ -29,11 +29,13 @@ while True:
         input("press any key to continue.")
 
     print(f"the video title is {yt.title.upper()}")
-    download_q = input("Do you wish to download the video?").lower()
+    download_q = input("Do you wish to download the video? ").lower()
     if download_q in ("no","n","","exit"):
         sys.exit()
     if download_q in ("yes","y"):
-        audio_q = input("Do you wish to download the audio version only?").lower()
+
+        specify_directory = input("Do you wish to specify the directory of the saved video? (Leave blank for the folder of the python script) ")
+        audio_q = input("Do you wish to download the audio version only? ").lower()
         if audio_q in ("yes","y"):
             audio_only()
         else:
